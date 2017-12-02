7d09a0e837c18dc287db0ad03978ec2f40e13f56
Maurizio Lupo <maurizio.lupo@gmail.com>
2017-12-02 19:09:46 +0000

    parser wip

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 obj-path-expression-parser
 ==========================
+A path expression is a string that expands to an array of arrays. Each array is a path to a value in an object.
+For example:
+```
+"a.b,c.d"
+```
+expands to:
+```
+[['a', 'b'], ['c', 'd']]
+```
+
+Features:
+=========
+
+Multiple expressions
+--------------------
+
+Regular expression matching
+---------------------------
+
+Slice notation
+--------------
+
+Filter
+------
+
+Escaping
+--------
+
+Nested Expression
+-----------------
+
+Syntax and parser internals
+===========================
+
+Path Expression
+---------------
+It is a comma separated list of paths. Gets parsed into an iterator of iterators (paths).
+Example:
+```
+"a.b.c,d.e,f.g" -> [['a', 'b', 'c'], ['d', 'e'], ['f', 'g']]
+```
+
+Path
+----
+It is a list of fragments. There are many types of fragments:
+* string fragment
+* reg exp fragment
+* slice fragment
+* custom fragment
+* nested path expression
+
+Glob fragment
+-------------
+It is separated by dots:
+```
+"a" -> StringFragment "a"
+```
+or
+```
+[a]
+```
+
+Slice fragment
+--------------
+
+Custom fragment
+---------------
+```
+{equal(arg1, arg2)}
+```
+
+Nested path expression
+----------------------
+```
+a(b,c(1.2,3)) -> [['a', 'b'], ['a', 'c', '1', '2'], ['a', 'c', '3']]
+```
+
+Escape rules
+------------
---
+++ tests/parser.test.js
@@ -0,0 +1,161 @@
+/* eslint-env node, mocha */
+const { assert } = require('chai');
+const parse = require('../lib/parser');
+
+describe.only('parse', () => {
+  it('throws an exception if no string', () =>
+    assert.throws(() => parse({}), /should be a string/));
+
+  it('throws an exception if begins with parenthesis', () =>
+    assert.throws(() => parse(']'), /can't start with/));
+
+  it('throws an exception leaving open parenthesis', () => {
+    assert.throws(() => parse('{1'), /requires "}"/);
+    assert.throws(() => parse('[1'), /requires "]"/);
+    assert.throws(() => parse('(1'), /should end with "\)"/);
+  });
+
+  it('throws an exception with spaces in unescaped fragments', () =>
+    assert.throws(() => parse('hello world'), /Spaces are not allowed/));
+
+  describe('unescapedFragment', () => {
+    it('convert simple path', () => {
+      const a = parse('hello.world');
+      assert.deepEqual(a, {
+        _type: 'pathExpressions',
+        expressions: [
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'unescapedFragment', fragment: 'hello' },
+              { _type: 'unescapedFragment', fragment: 'world' }
+            ]
+          }
+        ]
+      });
+    });
+
+    it('convert simple path (single item)', () => {
+      const a = parse('hello');
+      assert.deepEqual(a, {
+        _type: 'pathExpressions',
+        expressions: [
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'unescapedFragment', fragment: 'hello' }
+            ]
+          }
+        ]
+      });
+    });
+
+    it('convert 2 simple paths', () => {
+      const a = parse('hello.world,1.2');
+      assert.deepEqual(a, {
+        _type: 'pathExpressions',
+        expressions: [
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'unescapedFragment', fragment: 'hello' },
+              { _type: 'unescapedFragment', fragment: 'world' }
+            ]
+          },
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'unescapedFragment', fragment: '1' },
+              { _type: 'unescapedFragment', fragment: '2' }
+            ]
+          }
+        ]
+      });
+    });
+  });
+
+  describe('escapedFragment', () => {
+    it('parse escaped', () => {
+      const a = parse('hello[world],[1]2');
+      assert.deepEqual(a, {
+        _type: 'pathExpressions',
+        expressions: [
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'unescapedFragment', fragment: 'hello' },
+              { _type: 'escapedFragment', fragment: 'world' }
+            ]
+          },
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'escapedFragment', fragment: '1' },
+              { _type: 'unescapedFragment', fragment: '2' }
+            ]
+          }
+        ]
+      });
+    });
+
+    it('parse adjacent escaped', () => {
+      const a = parse('[1 x][2][3]');
+      assert.deepEqual(a, {
+        _type: 'pathExpressions',
+        expressions: [
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'escapedFragment', fragment: '1 x' },
+              { _type: 'escapedFragment', fragment: '2' },
+              { _type: 'escapedFragment', fragment: '3' }
+            ]
+          }
+        ]
+      });
+    });
+  });
+
+  describe('customFragment', () => {
+    it('parse escaped', () => {
+      const a = parse('hello{world},{1}2[3]');
+      assert.deepEqual(a, {
+        _type: 'pathExpressions',
+        expressions: [
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'unescapedFragment', fragment: 'hello' },
+              { _type: 'customFragment', fragment: 'world' }
+            ]
+          },
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'customFragment', fragment: '1' },
+              { _type: 'unescapedFragment', fragment: '2' },
+              { _type: 'escapedFragment', fragment: '3' }
+            ]
+          }
+        ]
+      });
+    });
+
+    it('parse adjacent escaped', () => {
+      const a = parse('{1 x}{2}{3}');
+      assert.deepEqual(a, {
+        _type: 'pathExpressions',
+        expressions: [
+          {
+            _type: 'pathExpression',
+            expression: [
+              { _type: 'customFragment', fragment: '1 x' },
+              { _type: 'customFragment', fragment: '2' },
+              { _type: 'customFragment', fragment: '3' }
+            ]
+          }
+        ]
+      });
+    });
+  });
+});
